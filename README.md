3cbf6c11503e8e3d74b50882759935253ee689d5
Vlad <vladgrichan@gmail.com>
2021-02-04 22:06:59 +0000

    set all todos

--- README.md
+++ README.md
@@ -1,24 +1,33 @@
 # vue-vuex-practice-0
 
+following: https://www.youtube.com/watch?v=5lVQgZzLMHc&t=1564s
+
+##
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,13 +1,10 @@
 // for actions and mutations getters setters
 // import axios from "axios";
 
+import axios from "axios";
+
 const state = {
-  todos: [
-    {
-      id: 1,
-      title: "todo 1"
-    }
-  ]
+  todos: []
 };
 
 const getters = {
@@ -16,9 +13,20 @@ const getters = {
   }
 };
 
-const actions = {};
+// action calls the state via commit
+const actions = {
+  async fetchTodos({ commit }) {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/todos"
+    );
+    commit("setTodos", response.data);
+  }
+};
 
-const mutations = {};
+// mutations mutate the state
+const mutations = {
+  setTodos: (state, todos) => (state.todos = todos)
+};
 
 export default {
   state: state,
